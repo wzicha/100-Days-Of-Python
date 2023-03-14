afcefa3370af4dd16bf20e1c562c8b466a182dce
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-13 22:51:08 -0400

    Miles to Kilometers Converter Program Project (Draft #2)
    Taking longer than anticipated. Learning how to use the grid to move buttons around/properly place them.

--- Day27/main.py
+++ Day27/main.py
@@ -1,36 +1,36 @@
 from tkinter import *
 
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.title("Miles to Kilometers Converter")
+window.minsize(width=200, height=200)
 
 # Label
 
-my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+km = 0
+my_label = Label(text=f"is equal to {km} km", font=("Arial", 12), compound="bottom")
 my_label.pack()
 
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
+
+# my_label["text"] = "New Text"
+# my_label.config(text="New Text")
 
 
 # Button
 
 def button_clicked():
-    print("I got clicked")
-    my_label.config(text=f"{input.get()}")
+    miles = input.get()
+    km = int(miles) * 1.60934
+    km = round(float(km) / 100, 2)
+    km = int(km * 100)
+    print(f"{miles} miles are equal to {km} km")
+    my_label.config(text=f"is equal to {km} km", font=("Arial", 12), compound="bottom")
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
-
-#Entry
-input = Entry(width=10)
+# Entry
+input = Entry(width=10, justify="center")
 input.pack()
-print(input.get())
 
-
-
-
-
+button = Button(text="Calculate", command=button_clicked)
+button.pack()
 
 window.mainloop()
