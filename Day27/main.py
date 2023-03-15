b4357617acce9c80c6ed5561f49dff88cd143d89
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-14 22:03:16 -0400

    Mile to Kilometers Converter Project Completed
    Figured out how to use the place() function... Could have used grid() to make the widget more concise. It works and is formatted pretty well, could be better.

--- Day27/main.py
+++ Day27/main.py
@@ -2,13 +2,16 @@ from tkinter import *
 
 window = Tk()
 window.title("Miles to Kilometers Converter")
-window.minsize(width=200, height=200)
+window.minsize(width=300, height=50)
 
-# Label
+# Labels
+
 
 km = 0
-my_label = Label(text=f"is equal to {km} km", font=("Arial", 12), compound="bottom")
-my_label.pack()
+miles_label = Label(text=f" Miles", font=("Arial", 12))
+miles_label.place(x=185, y=28)
+main_label = Label(text=f"is equal to    {km}    km", font=("Arial", 12))
+main_label.place(x=50, y=50)
 
 
 # my_label["text"] = "New Text"
@@ -22,15 +25,19 @@ def button_clicked():
     km = int(miles) * 1.60934
     km = round(float(km) / 100, 2)
     km = int(km * 100)
-    print(f"{miles} miles are equal to {km} km")
-    my_label.config(text=f"is equal to {km} km", font=("Arial", 12), compound="bottom")
+    print(f"{miles} miles is equal to    {km}    km")
+    main_label.config(text=f"is equal to    {km}    km", font=("Arial", 12))
 
 
-# Entry
+# Entry (textbox)
 input = Entry(width=10, justify="center")
-input.pack()
+input.place(x=120, y=30)
+# input.pack()
 
 button = Button(text="Calculate", command=button_clicked)
-button.pack()
+button.place(x=120, y=85)
+# button.pack()
+
+
 
 window.mainloop()
