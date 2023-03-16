14f416cb40444c0546854bc3e57cfff56d5259d4
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-15 23:22:34 -0400

    Draft of the count down mechanism

--- Day28/main.py
+++ Day28/main.py
@@ -10,11 +10,20 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,23 +35,25 @@ window.grid_columnconfigure(index=3)
 window.grid_rowconfigure(index=5)
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# Labels and placement (image, timer text and checkmark)
+# Labels and placement (Timer text, Image + countdown text and checkmark)
 
 timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 35, "bold"), highlightthickness=0)
 timer_label.grid(row=1, column=2)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(103, 135, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=2, column=2)
+
 checkmark = Label(text="✓", fg=GREEN, highlightthickness=0)
 checkmark.grid(row=4, column=2)
 
 # Create Start & Reset Buttons
 
-start_button = Button(text="Start", font=("Times New Roman", 12, "bold"))
+start_button = Button(text="Start", font=("Times New Roman", 12, "bold"), command=start_timer)
 start_button.grid(row=3, column=1)
-reset_button = Button(text="Reset",  font=("Times New Roman", 12, "bold"))
+reset_button = Button(text="Reset", font=("Times New Roman", 12, "bold"))
 reset_button.grid(row=3, column=3)
 
 window.mainloop()
