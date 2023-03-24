30e0a73f25de4d47ff0c476bf91effa07fcdb022
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-23 23:14:00 -0400

    Part 3 Complete - Added card flipping logic
    Following the first card, the card flips automatically after 3 seconds of no response

--- Day31/main.py
+++ Day31/main.py
@@ -5,18 +5,41 @@ import random
 
 french_words_file = pandas.read_csv('data/french_words.csv')
 french_words = pandas.DataFrame(french_words_file)
-french_word_test = french_words.loc[random.randint(1, 102), 'French']
+random_value = random.randint(1, 102)
+french_word_test = french_words.loc[random_value, 'French']
+english_word_test = french_words.loc[random_value, 'English']
 
 
 def generate_word():
     global french_word_test
+    global english_word_test
+    global back_label
+    back_label.grid_forget()
+    # front_label = Label(image=card_front, borderwidth=0, highlightthickness=0, background=BACKGROUND_COLOR)
+    # front_label.grid(column=0, row=0, columnspan=2, rowspan=1)
+    # front_label.lift()
+    language.config(text="French")
     french_words_file = pandas.read_csv('data/french_words.csv')
     french_words = pandas.DataFrame(french_words_file)
-    new_word = french_words.loc[random.randint(1, 102), 'French']
+    random_value = random.randint(1, 102)
+    new_word = french_words.loc[random_value, 'French']
+    english_word_test = french_words.loc[random_value, 'English']
     french_word_test = new_word
     word_presented.config(text=f"{french_word_test}")
     window.update()
+    window.after(3000, flip_card)
+    window.update()
 
+def flip_card():
+    global back_label
+    # front_label.grid_forget()
+    back_label = Label(image=card_back, borderwidth=0, highlightthickness=0, background='GREY')
+    back_label.grid(column=0, row=0, columnspan=2, rowspan=1)
+    language.config(text="English", bg="#B1DDC6", highlightthickness=0)
+    word_presented.config(text=f"{english_word_test}", bg="#B1DDC6", highlightthickness=0)
+    language.lift()
+    word_presented.lift()
+    window.update()
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -34,6 +57,8 @@ card_back = PhotoImage(file='images/card_back.png')
 front_label = Label(image=card_front, borderwidth=0, highlightthickness=0, background=BACKGROUND_COLOR)
 front_label.grid(column=0, row=0, columnspan=2, rowspan=1)
 front_label.lift()
+
+back_label = Label(image=card_back, borderwidth=0, highlightthickness=0, background='GREY')
 
 language = Label(text="French", font=("Ariel", 40, "italic"), compound='center', anchor="center", highlightthickness=0,
                  background='white')
