098a27df7dfed18068c57930a812e457edc640b2
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-26 21:26:14 -0400

    Challenge 1 Completed - Send quote every Monday
    Redacted email and password. Sends a quote every Monday from the list quotes.txt

--- Day32/Birthday Wisher (Day 32) start/main.py
+++ Day32/Birthday Wisher (Day 32) start/main.py
@@ -1,22 +1,25 @@
-##################### Hard Starting Project ######################
+import datetime as dt
+import random
+import smtplib
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+with open("quotes.txt", "r") as data:
+    quotes = data.readlines()
+    quote = random.choice(quotes)
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+print(quote)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+# email
+# password
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+now = dt.datetime.now()
+day_of_week = now.weekday()
+print(day_of_week)
+# Monday = 0
 
-
-
+if day_of_week == 0:
+    print("Today is Monday")
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="wzicha@gmail.com", msg=f"Subject: Happy Monday\n\n{quote}")
+        connection.close()
