87292046731098c57e7cfb5a25bfbe5b488d29b1
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-03-28 20:23:09 -0400

    Challenge 2 Completed

--- Day32/main.py
+++ Day32/main.py
@@ -17,7 +17,7 @@ now = dt.datetime.now()
 month = now.month
 day = now.day
 
-birthdays_dict_new = {}
+# Create a dictionary from birthdays.csv
 # Create a dictionary from the dataframe with (month, day) tuples as keys
 # and data rows as values
 birthdays_dict = {}
@@ -30,31 +30,30 @@ for i, row in birthdays_df.iterrows():
 now = dt.datetime.now()
 
 # Extract the month and day from the current date
+# Only the month and day matter.
+
 current_month = now.month
 current_day = now.day
 
 # Check if today's date matches a birthday in the birthdays dictionary
 if (current_month, current_day) in birthdays_dict:
-    print("Today is a birthday! Data:", birthdays_dict[(current_month, current_day)])
+    print("Today is a birthday!")
+    name = birthdays_dict[(current_month, current_day)]["name"]
+    email = birthdays_dict[(current_month, current_day)]["email"]
+    # Pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    variation = random.randint(1, 3)
+    with open(f'letter_templates/letter_{variation}.txt', 'r') as file:
+        text = file.read()
+        new_string = text.replace("[NAME]", f"{name}").replace("Angela", "Capy")
+        print(new_string)
 else:
     print("No birthdays today.")
 
-# HINT 1: Only the month and day matter.
 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-# HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+# Send the letter generated in step 3 to that person's email address.
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="wzicha@gmail.com", msg=f"Subject: Happy Birthday\n\n")
+    connection.sendmail(from_addr=my_email, to_addrs={email}, msg=f"Subject: Happy Birthday\n\n{new_string}")
     connection.close()
