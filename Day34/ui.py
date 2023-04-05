35fe0fd3d8bb38de213f30e118979e216e183a45
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-04-04 23:02:34 -0400

    Draft #2
    Working on UI through Tkinter

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,2 +1,20 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(background=THEME_COLOR)
+        canvas = Canvas(self.window, width=350, height=500, bg=THEME_COLOR)
+        self.window.columnconfigure(index=2)
+        self.window.rowconfigure(index=3)
+        self.score = Label(text="Score: 0", padx=20, pady=20, font=("Arial", "12"), fg="white", bg=THEME_COLOR)
+        self.score.grid(column=1, row=0)
+        self.question = Label(text="qqq", font=("Arial", "20", "italic"), height=250, width=300, justify="center")
+        self.question.grid(column=0, row=1)
+
+        self.window.mainloop()
