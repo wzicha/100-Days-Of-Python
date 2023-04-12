c23fb9f733db4d85351e99598efe760b73ab16fa
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-04-11 22:43:34 -0400

    Day 34 Completed

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 import os
 import html
@@ -13,6 +14,7 @@ class QuizInterface:
         self.quiz = quiz_brain
         self.window = Tk()
         self.score = 0
+        self.question_count = 0
         self.window.geometry("360x550")
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -30,11 +32,11 @@ class QuizInterface:
         self.question_text_label.grid(column=0, row=1, columnspan=2, pady=50)
 
         self.true_image = PhotoImage(file=os.path.join(image_dir, "true.png"))
-        self.true_button = Button(image=self.true_image, command=lambda: self.check_answer("True"))
+        self.true_button = Button(image=self.true_image, command=self.true_pressed)
         self.true_button.grid(column=0, row=2, padx=30, sticky="E")
 
         self.false_image = PhotoImage(file=os.path.join(image_dir, "false.png"))
-        self.false_button = Button(image=self.false_image, command=lambda: self.check_answer("False"))
+        self.false_button = Button(image=self.false_image, command=self.false_pressed)
         self.false_button.grid(column=1, row=2, padx=30, sticky="W")
 
         self.window.grid_columnconfigure(0, weight=1)
@@ -46,21 +48,39 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        if self.question_count == 10:
+            self.window.quit()
+            return
+        self.question_count += 1
         q_text = self.quiz.next_question()
         self.question_text_label.config(text=q_text)
 
-    def check_answer(self, user_answer):
+    def true_pressed(self):
+        self.quiz.check_answer("True")
         correct_answer = self.quiz.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
-                self.score += 1
-                self.score_label.config(text=f"Score: {self.score}")
-                print("You got it right!")
+        if "true" == correct_answer.lower():
+            self.score += 1
+            self.score_label.config(text=f"Score: {self.score}")
+            self.question_text_label.config(bg="Green")
+            self.window.after(500, lambda: self.question_text_label.config(bg="White"))
         else:
-            print("That's wrong.")
-            self.get_next_question()
-        if self.quiz.next_question() is None:
-                print("You've completed the quiz")
-                print(f"Your final score was: {self.score}/{self.quiz.question_number}")
+            self.question_text_label.config(bg="Red")
+            self.window.after(500, lambda: self.question_text_label.config(bg="White"))
+        self.get_next_question()
+
+    def false_pressed(self):
+        self.quiz.check_answer("False")
+        correct_answer = self.quiz.current_question.answer
+        if "false" == correct_answer.lower():
+            self.score += 1
+            self.score_label.config(text=f"Score: {self.score}")
+            self.question_text_label.config(bg="Green")
+            self.window.after(500, lambda: self.question_text_label.config(bg="White"))
+        else:
+            self.question_text_label.config(bg="Red")
+            self.window.after(500, lambda: self.question_text_label.config(bg="White"))
+        self.get_next_question()
+
 
 
 
