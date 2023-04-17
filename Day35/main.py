d7845a38168798c054c982a1010d846e8ffc2413
William Zicha <74578451+wzicha@users.noreply.github.com>
2023-04-16 22:26:11 -0400

    Challenge 1 Completed (Check if it will rain in next 12h)

--- Day35/main.py
+++ Day35/main.py
@@ -1,21 +1,28 @@
 import requests
-import datetime
 
 lat = 45.500940  # Your latitude
 lon = -73.572700  # Your longitude
-#api key
+#API_KEY
 weather_params = {
     "lat": lat,
     "lon": lon,
     "appid": API_key,
+    "exclude": "current,minutely,daily"
 }
 OWM_Endpoint = "https://api.openweathermap.org/data/2.8/onecall"
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-# response.raise_for_status()
-data = response.json()
+response.raise_for_status()
+weather_data = response.json()
 
 print(response.status_code)
-print(data)
+print(weather_data)
 
+print(weather_data['hourly'][0]['weather'])
+print(weather_data['hourly'][0]['weather'][0]['main'])
 
+counter = 0
+for data in weather_data:
+    while counter < 12:
+        counter += 1
+        print(weather_data['hourly'][counter]['weather'][0]['main'])
